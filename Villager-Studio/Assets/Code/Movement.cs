495f80728973239eb0604f8d57e6e3de7b7f310d
Restored9460 <123942221+Restored9460@users.noreply.github.com>
2024-03-11 12:51:37 +0000

    Movement

--- Villager-Studio/Assets/Code/Movement.cs
+++ Villager-Studio/Assets/Code/Movement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private float speed = 10;
+    private float rotationSpeed = 180;
+
+    CharacterController characterController;
+    Vector3 moveVelocity;
+    Vector3 turnVelocity;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+
+        moveVelocity = transform.forward * speed * vInput;
+        turnVelocity = transform.up * rotationSpeed * hInput;
+
+        characterController.Move(moveVelocity * Time.deltaTime);
+        transform.Rotate(turnVelocity * Time.deltaTime);
+        this.transform.position = new Vector3(this.transform.position.x, 2.1407f, this.transform.position.z);
     }
+
 }
